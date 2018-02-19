d0e033c9834a604573fba7c28efa31d6e156eae4
Welton Antonio Gomes <wagomes@eerp.local>
2018-02-19 14:22:03 -0300

    Adicionadas as soluções do tópico 2.

--- 02_Condicionais_e_de_Repetição/ex01.py
+++ 02_Condicionais_e_de_Repetição/ex01.py
@@ -15,3 +15,30 @@ Valor               Desconto (%)
 >= 1000             8
 ----------------------------------
 '''
+import locale
+locale.setlocale(locale.LC_ALL,"")
+
+print('Calculadora de descontos')
+
+nome = input('Digite o nome do produto: ')
+valor_original = float(input('Digite o valor do produto: ').replace(',','.'))
+
+while(valor_original<0):
+    valor_original = float(input('Valor inválido. Digite novo valor: ').replace(',','.'))
+
+if(valor_original<50):
+    desconto = 0
+elif(valor_original>=50 and valor_original<200):
+    desconto = 0.05
+elif(valor_original>=200 and valor_original<500):
+    desconto = 0.06
+elif(valor_original>=500 and valor_original<1000):
+    desconto = 0.07
+else:
+    desconto = 0.08
+
+valor_desconto = valor_original * (1-desconto)
+
+print('Produto: {}'.format(nome))
+print('Valor original: {}'.format(locale.currency(valor_original)))
+print('Valor com desconto: {}'.format(locale.currency(valor_desconto)))
--- 02_Condicionais_e_de_Repetição/ex03.py
+++ 02_Condicionais_e_de_Repetição/ex03.py
@@ -5,3 +5,12 @@ de qualquer número.
 - O usuário fornece o número desejado e a classe
 apresenta a relação de todos os cálculos de 1 a 10.
 '''
+import locale
+locale.setlocale(locale.LC_ALL,"")
+
+print('Gerador de tabuadas')
+
+numero = int(input('Número que deseja gerar a tabuada: '))
+
+for i in range(11):
+    print('{} x {} = {}'.format(numero,i,i*numero))
