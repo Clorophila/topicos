12b39fc68d6c8b1598faf1c51afae48c2b3f318b
WELTON GOMES <2840481713038@fatecrp.local>
2018-03-01 20:11:26 -0300

    Resolvido exercicio 01 de Tuplas

--- 05_Tuplas/ex01.py
+++ 05_Tuplas/ex01.py
@@ -9,3 +9,30 @@ elementos numéricos inteiros, calcule e mostre:
     • O menor número
     • A quantidade de números positivos
 '''
+lista = []
+
+for n in range(6):
+    while True:
+        try:
+            valor = int(input('Digite o valor do número {}: '.format(n+1)))
+            break
+        except:
+            print('Valor digitado inválido. Digite um número inteiro.')
+    lista.append(valor)
+tupla = tuple(lista)
+pares = 0
+impares = 0
+soma = sum(tupla)
+menor = min(tupla)
+maior = max(tupla)
+for valor in tupla:
+    if valor%2==0:
+        pares+=1
+    else:
+        impares+=1
+print('Tupla dos valores: {}'.format(tupla))
+print('Quantidade de pares = {}'.format(pares))
+print('Quantidade de impares = {}'.format(impares))
+print('Soma dos números digitados = {}'.format(soma))
+print('Menor número digitado = {}'.format(menor))
+print('Maior número digitado = {}'.format(maior))
