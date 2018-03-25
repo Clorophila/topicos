b057ee152caf347044b8a5bbbf80bfedf947041b
clorophila <clorofila@gmail.com>
2018-03-24 23:06:49 -0300

    Resolvido exercício 04

--- EX02/ex04.py
+++ EX02/ex04.py
@@ -2,3 +2,16 @@
 Exercício 4: Escreva um programa que recebe
 sequencia e calcula a quantidade de letras e dígitos.
 '''
+lista = input().split()
+
+letras = 0
+digitos = 0
+
+for item in lista:
+    if item.isnumeric():
+        digitos += 1
+    elif item.isalpha():
+        letras += 1
+
+print('Quantidade de letras: {}'.format(letras))
+print('Quantidade de dígitos: {}'.format(digitos))
